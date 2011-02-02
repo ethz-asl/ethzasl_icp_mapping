507a6f8a57c31bcbaa72fd0dcaa2dd8998309d95
Stéphane Magnenat <stephane@magnenat.net>
2011-02-02 18:18:23 +0100

    Returned to experimental work, optimised libpointmatcher for tracker, gives 3x speed increase.

--- modular_cloud_matcher/src/modular_cloud_matcher.cpp
+++ modular_cloud_matcher/src/modular_cloud_matcher.cpp
@@ -111,18 +111,14 @@ MSA::DataPointsFilter* createSamplingSurfaceNormalDataPointsFilter(const string&
 MSA::DataPointsFilter* createRandomSamplingDataPointsFilter(const string& root)
 {
 	return new MSA::RandomSamplingDataPointsFilter(
-		getParam(root + "prob", 0.5),
-		getParam(root + "enablePreFilter", true),
-		getParam(root + "enableStepFilter", false)
+		getParam(root + "prob", 0.5)
 	);
 }
 
 MSA::DataPointsFilter* createFixstepSamplingDataPointsFilter(const string& root)
 {
 	return new MSA::FixstepSamplingDataPointsFilter(
-		getParam(root + "step", 20),
-		getParam(root + "enablePreFilter", true),
-		getParam(root + "enableStepFilter", false)
+		getParam(root + "step", 20)
 	);
 }
 
@@ -189,120 +185,66 @@ MSA::Inspector* createVTKFileInspector(const string& root)
 typedef MSA::TransformationParameters TP;
 typedef MSA::DataPoints DP;
 
-MSA::Strategy p;
+MSA::ICPSequence icp(3);
 
-void buildStrategy()
+void populateParameters(void)
 {
+	// global parameters
+	icp.ratioToSwitchKeyframe = getParam("ratioToSwitchKeyframe", 0.8);
+	
+	// icp parameters
 	const int transformationCount(getParam<int>("transformationCount", 1));
 	const int readingDataPointsFilterCount(getParam<int>("readingDataPointsFilterCount", 1));
-	const int referenceDataPointsFilterCount(getParam<int>("referenceDataPointsFilterCount", 1));
+	const int keyframeDataPointsFilterCount(getParam<int>("keyframeDataPointsFilterCount", 1));
 	const int featureOutlierFilterCount(getParam<int>("featureOutlierFilterCount", 1));
 	const int transformationCheckerCount(getParam<int>("transformationCheckerCount", 1));
 	
 	for (int i = 0; i < readingDataPointsFilterCount; ++i)
 	{
 		string root((boost::format("readingDataPointsFilter/%1%") % i).str());
-		p.readingDataPointsFilters.push_back(REG(DataPointsFilter).create(getParam<string>(root+"/name", "FixstepSamplingDataPointsFilter"), root + "/"));
+		icp.readingDataPointsFilters.push_back(REG(DataPointsFilter).create(getParam<string>(root+"/name", "FixstepSamplingDataPointsFilter"), root + "/"));
 	}
-	for (int i = 0; i < referenceDataPointsFilterCount; ++i)
+	for (int i = 0; i < keyframeDataPointsFilterCount; ++i)
 	{
-		string root((boost::format("referenceDataPointsFilter/%1%") % i).str());
-		p.referenceDataPointsFilters.push_back(REG(DataPointsFilter).create(getParam<string>(root+"/name", "SamplingSurfaceNormalDataPointsFilter"), root + "/"));
+		string root((boost::format("keyframeDataPointsFilter/%1%") % i).str());
+		icp.keyframeDataPointsFilters.push_back(REG(DataPointsFilter).create(getParam<string>(root+"/name", "SamplingSurfaceNormalDataPointsFilter"), root + "/"));
 	}
 	for (int i = 0; i < transformationCount; ++i)
 	{
 		string root((boost::format("transformations/%1%") % i).str());
-		p.transformations.push_back(REG(Transformation).create(getParam<string>(root+"/name", "TransformFeatures"), root + "/"));
+		icp.transformations.push_back(REG(Transformation).create(getParam<string>(root+"/name", "TransformFeatures"), root + "/"));
 	}
-	p.matcher = REG(Matcher).create(getParam<string>("matcher/name", "KDTreeMatcher"), "matcher/");
+	icp.matcher = REG(Matcher).create(getParam<string>("matcher/name", "KDTreeMatcher"), "matcher/");
 	for (int i = 0; i < featureOutlierFilterCount; ++i)
 	{
 		string root((boost::format("featureOutlierFilters/%1%") % i).str());
-		p.featureOutlierFilters.push_back(REG(FeatureOutlierFilter).create(getParam<string>(root+"/name", "MedianDistOutlierFilter"), root + "/"));
+		icp.featureOutlierFilters.push_back(REG(FeatureOutlierFilter).create(getParam<string>(root+"/name", "MedianDistOutlierFilter"), root + "/"));
 	}
-	p.descriptorOutlierFilter = REG(DescriptorOutlierFilter).create(getParam<string>("descriptorOutlierFilter/name", "NullDescriptorOutlierFilter"), "descriptorOutlierFilter/");
-	p.errorMinimizer = REG(ErrorMinimizer).create(getParam<string>("errorMinimizer/name", "PointToPlaneErrorMinimizer"), "errorMinimizer/");
+	icp.descriptorOutlierFilter = REG(DescriptorOutlierFilter).create(getParam<string>("descriptorOutlierFilter/name", "NullDescriptorOutlierFilter"), "descriptorOutlierFilter/");
+	icp.errorMinimizer = REG(ErrorMinimizer).create(getParam<string>("errorMinimizer/name", "PointToPlaneErrorMinimizer"), "errorMinimizer/");
 	for (int i = 0; i < transformationCheckerCount; ++i)
 	{
 		string root((boost::format("transformationCheckers/%1%") % i).str());
-		p.transformationCheckers.push_back(REG(TransformationChecker).create(getParam<string>(root+"/name", "ErrorTransformationChecker"), root + "/"));
+		icp.transformationCheckers.push_back(REG(TransformationChecker).create(getParam<string>(root+"/name", "ErrorTransformationChecker"), root + "/"));
 	}
-	p.inspector = REG(Inspector).create(getParam<string>("inspector/name", "Inspector"), "inspector");
-	//p.inspector = REG(Inspector).create(getParam<string>("inspector", "VTKFileInspector"), "inspector");
-	p.outlierMixingWeight = getParam<double>("outlierMixingWeight", 1);
+	icp.inspector = REG(Inspector).create(getParam<string>("inspector/name", "Inspector"), "inspector");
+	//icp.inspector = REG(Inspector).create(getParam<string>("inspector", "VTKFileInspector"), "inspector");
+	icp.outlierMixingWeight = getParam<double>("outlierMixingWeight", 1);
 }
 
-bool matchClouds(modular_cloud_matcher::MatchClouds::Request& req,
-				 modular_cloud_matcher::MatchClouds::Response& res)
-{
-	//ros::param::param("featureTransformation", default_param, "default_value");
-	
-	// create labels
-	DP::Labels labels;
-	labels.push_back(DP::Label("x", 1));
-	labels.push_back(DP::Label("y", 1));
-	labels.push_back(DP::Label("z", 1));
-	labels.push_back(DP::Label("pad", 1));
-	
-	// create reference
-	DP ref(DP::Features(4, req.reference.points.size()), labels);
-	for (size_t i = 0; i < req.reference.points.size(); ++i)
-	{
-		ref.features.coeffRef(0, i) = req.reference.points[i].x;
-		ref.features.coeffRef(1, i) = req.reference.points[i].y;
-		ref.features.coeffRef(2, i) = req.reference.points[i].z;
-		ref.features.coeffRef(3, i) = 1;
-	}
-	ROS_INFO_STREAM("reference has " << ref.features.cols() << " points");
-	
-	// create readings
-	DP data(DP::Features(4, req.readings.points.size()), labels);
-	for (size_t i = 0; i < req.readings.points.size(); ++i)
-	{
-		data.features.coeffRef(0, i) = req.readings.points[i].x;
-		data.features.coeffRef(1, i) = req.readings.points[i].y;
-		data.features.coeffRef(2, i) = req.readings.points[i].z;
-		data.features.coeffRef(3, i) = 1;
-	}
-	ROS_INFO_STREAM("readings has " << data.features.cols() << " points");
-	
-	// do icp
-	const TP initialTransform(TP::Identity(data.features.rows(), data.features.rows()));
-	const TP finalTransform = MSA::icp(initialTransform, data, ref, p);
-	const Eigen::Quaternion<double> quat(Eigen::Matrix3d(finalTransform.block(0,0,3,3)));
-	
-	res.transform.translation.x = finalTransform.coeff(0,3);
-	res.transform.translation.y = finalTransform.coeff(1,3);
-	res.transform.translation.z = finalTransform.coeff(2,3);
-	res.transform.rotation.x = quat.x();
-	res.transform.rotation.y = quat.y();
-	res.transform.rotation.z = quat.z();
-	res.transform.rotation.w = quat.w();
-	
-	return true;
-}
-
-static bool first(true);
 static DP keyFrameCloud;
 static TP keyFrameTransform(TP::Identity(4, 4));
 static TP curTransform(TP::Identity(4, 4));
-static long long cnt(0);
 
 static Eigen::Vector3d sumRot(Eigen::Vector3d::Zero(3));
 static Eigen::Vector3d sumTrans(Eigen::Vector3d::Zero(3));
 
-static double ratioToSwitchKeyframe(-1);
 static double maxSensorDist(-1);
 static string fixedFrame;
 static string sensorFrame;
 
 void gotCloud(const sensor_msgs::PointCloud& cloudMsg)
 {
-	// FIXME: investigate broken cloud on first call!
-	cnt++;
-	if (cnt == 1)
-		return;
-	
 	// create labels
 	DP::Labels labels;
 	labels.push_back(DP::Label("x", 1));
@@ -344,68 +286,29 @@ void gotCloud(const sensor_msgs::PointCloud& cloudMsg)
 	}
 	ROS_INFO_STREAM("Got " << cloudMsg.points.size() << " points (" << goodCount << " goods)");
 	
-  double imageRatio = (double)goodCount / (double)cloudMsg.points.size();
- 
-  //TODO: put that as parameter, tricky to set...
-  if (goodCount < 8000)
-  {
-    ROS_ERROR_STREAM("Partial image! Missing " << 100 - imageRatio*100.0 << "% of the image (received " << goodCount << ")");
-    return;
-  }
-
-
-	if (first)
+	const double imageRatio = (double)goodCount / (double)cloudMsg.points.size();
+	
+	//TODO: put that as parameter, tricky to set...
+	if (goodCount < 8000)
 	{
-		keyFrameCloud = dp;
-		first = false;
+		ROS_ERROR_STREAM("Partial image! Missing " << 100 - imageRatio*100.0 << "% of the image (received " << goodCount << ")");
 		return;
 	}
 	
-	DP tempDP(dp);
-	DP tempKeyFrameCloud(keyFrameCloud);
+	// call icp
 	try 
 	{
-		const TP finalTransform = MSA::icp(curTransform, tempDP, tempKeyFrameCloud, p);
-		
-		/*curTransform *= finalTransform;
-		keyFrameTransform = curTransform;
-		keyFrameCloud = dp;*/
-		
-		/*
-		NOTE: For using the keyframe mode, you have to removed the outlier
-		based on distance. This could be a typical configuration for this:
-		<param name="featureOutlierFilterCount" value="2" />
-		<param name="featureOutlierFilters/0/name" value="MaxDistOutlierFilter" />
-		<param name="featureOutlierFilters/0/maxDist" value=".3" />
-		<param name="featureOutlierFilters/1/name" value="MedianDistOutlierFilter" />
-		<param name="featureOutlierFilters/1/factor" value="4" />
-		*/
-		if (p.errorMinimizer->getWeightedPointUsedRatio() < ratioToSwitchKeyframe)
-		{
-			ROS_INFO("New keyframe");
-			// new keyframe
-			keyFrameTransform *= finalTransform;
-			keyFrameCloud = dp;
-			curTransform = TP::Identity(4,4);
-		}
-		else
-		{
-			curTransform = finalTransform;
-		}
+		icp(dp);
+		ROS_INFO_STREAM("match ratio: " << icp.errorMinimizer->getWeightedPointUsedRatio() << endl);
 	}
 	catch (MSA::ConvergenceError error)
 	{
 		ROS_WARN_STREAM("ICP failed to converge: " << error.what());
 	}
 	
-	const TP globalTransform(keyFrameTransform * curTransform);
-	
-	//abort();
-	
-	ROS_INFO_STREAM("match ratio: " << p.errorMinimizer->getWeightedPointUsedRatio() << endl);
-	
+	// broadcast transform
+	const TP globalTransform(icp.getTransform());
 	const Eigen::Quaternion<double> quat(Eigen::Matrix3d(globalTransform.block(0,0,3,3)));
-	
 	//cerr << "pos:\n" << curTransform << endl;
 	const double a0(atan2(2*(quat.w() * quat.x() + quat.y() * quat.z()), 1-2*(quat.x() * quat.x() + quat.y() * quat.y())));
 	const double a1(asin(2*(quat.w() * quat.y() - quat.z() * quat.x())));
@@ -468,23 +371,68 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "cloud_matcher");
 	ros::NodeHandle n;
 	
-	buildStrategy();
+	string cloudTopic(getParam<string>("cloudTopic", "/camera/depth/points"));
+	ros::Subscriber sub = n.subscribe(cloudTopic, 1, gotCloud);
 	
-	if (argc > 1 && (strcmp(argv[1], "-topic") == 0))
-	{
-		string cloudTopic(getParam<string>("cloudTopic", "/camera/depth/points"));
-		ros::Subscriber sub = n.subscribe(cloudTopic, 1, gotCloud);
-		ratioToSwitchKeyframe = getParam("ratioToSwitchKeyframe", 0.8);
-		maxSensorDist = getParam("maxSensorDist", 2.5);
-		fixedFrame = getParam<string>("fixedFrame",  "/world");
-		sensorFrame = getParam<string>("sensorFrame",  "/openni_rgb_optical_frame");
-		ros::spin();
-	}
-	else
-	{
-		ros::ServiceServer service = n.advertiseService("matchClouds", matchClouds);
-		ros::spin();
-	}
+	maxSensorDist = getParam("maxSensorDist", 2.5);
+	fixedFrame = getParam<string>("fixedFrame",  "/world");
+	sensorFrame = getParam<string>("sensorFrame",  "/openni_rgb_optical_frame");
+	populateParameters();
+	
+	ros::spin();
 	
 	return 0;
 }
+
+
+#if 0
+bool matchClouds(modular_cloud_matcher::MatchClouds::Request& req,
+				 modular_cloud_matcher::MatchClouds::Response& res)
+{
+	//ros::param::param("featureTransformation", default_param, "default_value");
+	
+	// create labels
+	DP::Labels labels;
+	labels.push_back(DP::Label("x", 1));
+	labels.push_back(DP::Label("y", 1));
+	labels.push_back(DP::Label("z", 1));
+	labels.push_back(DP::Label("pad", 1));
+	
+	// create reference
+	DP ref(DP::Features(4, req.reference.points.size()), labels);
+	for (size_t i = 0; i < req.reference.points.size(); ++i)
+	{
+		ref.features.coeffRef(0, i) = req.reference.points[i].x;
+		ref.features.coeffRef(1, i) = req.reference.points[i].y;
+		ref.features.coeffRef(2, i) = req.reference.points[i].z;
+		ref.features.coeffRef(3, i) = 1;
+	}
+	ROS_INFO_STREAM("reference has " << ref.features.cols() << " points");
+	
+	// create readings
+	DP data(DP::Features(4, req.readings.points.size()), labels);
+	for (size_t i = 0; i < req.readings.points.size(); ++i)
+	{
+		data.features.coeffRef(0, i) = req.readings.points[i].x;
+		data.features.coeffRef(1, i) = req.readings.points[i].y;
+		data.features.coeffRef(2, i) = req.readings.points[i].z;
+		data.features.coeffRef(3, i) = 1;
+	}
+	ROS_INFO_STREAM("readings has " << data.features.cols() << " points");
+	
+	// do icp
+	const TP initialTransform(TP::Identity(data.features.rows(), data.features.rows()));
+	const TP finalTransform = MSA::icp(initialTransform, data, ref, p);
+	const Eigen::Quaternion<double> quat(Eigen::Matrix3d(finalTransform.block(0,0,3,3)));
+	
+	res.transform.translation.x = finalTransform.coeff(0,3);
+	res.transform.translation.y = finalTransform.coeff(1,3);
+	res.transform.translation.z = finalTransform.coeff(2,3);
+	res.transform.rotation.x = quat.x();
+	res.transform.rotation.y = quat.y();
+	res.transform.rotation.z = quat.z();
+	res.transform.rotation.w = quat.w();
+	
+	return true;
+}
+#endif
