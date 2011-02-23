8bbe38b8cca584c4754dd8284df538f95075b159
Stéphane Magnenat <stephane@magnenat.net>
2011-02-23 17:41:58 +0100

    Switche to diamondback.

--- modular_cloud_matcher/src/modular_cloud_matcher.cpp
+++ modular_cloud_matcher/src/modular_cloud_matcher.cpp
@@ -1,10 +1,13 @@
 #include "icp_chain_creation.h"
 
 #include "ros/ros.h"
-#include "sensor_msgs/PointCloud.h"
+#include "sensor_msgs/PointCloud2.h"
 #include "nav_msgs/Path.h"
-#include <tf/transform_broadcaster.h>
-#include <tf_conversions/tf_eigen.h>
+#include "pcl/point_types.h"
+#include "pcl/point_cloud.h" 
+#include "pcl/ros/conversions.h"
+#include "tf/transform_broadcaster.h"
+#include "tf_conversions/tf_eigen.h"
 #include "pointmatcher/PointMatcher.h"
 
 using namespace std;
@@ -27,7 +30,7 @@ class CloudMatcher
 	
 public:
 	CloudMatcher(ros::NodeHandle& n, const std::string &statFilePrefix);
-	void gotCloud(const sensor_msgs::PointCloud& cloudMsg);
+	void gotCloud(const sensor_msgs::PointCloud2& cloudMsg);
 };
 
 CloudMatcher::CloudMatcher(ros::NodeHandle& n, const std::string &statFilePrefix):
@@ -38,7 +41,7 @@ CloudMatcher::CloudMatcher(ros::NodeHandle& n, const std::string &statFilePrefix
 	startupDropCount(getParam("startupDropCount", 0)),
 	dropCount(0)
 {
-	const string cloudTopic(getParam<string>("cloudTopic", "/camera/depth/points"));
+	const string cloudTopic(getParam<string>("cloudTopic", "/camera/depth/points2"));
 	cloudSub = n.subscribe(cloudTopic, 1, &CloudMatcher::gotCloud, this);
 	
 	const string pathTopic(getParam<string>("path", "/tracker_path"));
@@ -49,7 +52,7 @@ CloudMatcher::CloudMatcher(ros::NodeHandle& n, const std::string &statFilePrefix
 	populateParameters(icp);
 }
 
-void CloudMatcher::gotCloud(const sensor_msgs::PointCloud& cloudMsg)
+void CloudMatcher::gotCloud(const sensor_msgs::PointCloud2& cloudMsg)
 {
 	if (dropCount < startupDropCount)
 	{
@@ -65,10 +68,13 @@ void CloudMatcher::gotCloud(const sensor_msgs::PointCloud& cloudMsg)
 	labels.push_back(DP::Label("pad", 1));
 	
 	// create data points
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	pcl::fromROSMsg (cloudMsg, cloud);
+
 	size_t goodCount(0);
-	for (size_t i = 0; i < cloudMsg.points.size(); ++i)
+	for (size_t i = 0; i < cloud.points.size(); ++i)
 	{
-		if (!isnan(cloudMsg.points[i].x))
+		if (!isnan(cloud.points[i].x))
 			++goodCount;
 	}
 	if (goodCount == 0)
@@ -79,26 +85,26 @@ void CloudMatcher::gotCloud(const sensor_msgs::PointCloud& cloudMsg)
 	
 	DP dp(DP::Features(4, goodCount), labels);
 	int dIndex(0);
-	for (size_t i = 0; i < cloudMsg.points.size(); ++i)
+	for (size_t i = 0; i < cloud.points.size(); ++i)
 	{
-		if (!isnan(cloudMsg.points[i].x))
+		if (!isnan(cloud.points[i].x))
 		{
-			dp.features.coeffRef(0, dIndex) = cloudMsg.points[i].x;
-			dp.features.coeffRef(1, dIndex) = cloudMsg.points[i].y;
-			dp.features.coeffRef(2, dIndex) = cloudMsg.points[i].z;
+			dp.features.coeffRef(0, dIndex) = cloud.points[i].x;
+			dp.features.coeffRef(1, dIndex) = cloud.points[i].y;
+			dp.features.coeffRef(2, dIndex) = cloud.points[i].z;
 			dp.features.coeffRef(3, dIndex) = 1;
 			++dIndex;
 		}
 	}
-	ROS_INFO_STREAM("Got " << cloudMsg.points.size() << " points (" << goodCount << " goods)");
+	ROS_INFO_STREAM("Got " << cloud.points.size() << " points (" << goodCount << " goods)");
 	
-	const double imageRatio = (double)goodCount / (double)cloudMsg.points.size();
+	const double imageRatio = (double)goodCount / (double)cloud.points.size();
 	
 	//TODO: put that as parameter, tricky to set...
 	if (goodCount < 10000)
 	{
 		ROS_ERROR_STREAM("Partial image! Missing " << 100 - imageRatio*100.0 << "% of the image (received " << goodCount << ")");
-		return;
+		//return;
 	}
 	
 	// call icp
@@ -114,10 +120,10 @@ void CloudMatcher::gotCloud(const sensor_msgs::PointCloud& cloudMsg)
 	
 	// broadcast transform
 	const TP globalTransform(icp.getTransform());
-	const Eigen::Quaternion<Scalar> quat(Matrix3(globalTransform.block(0,0,3,3)));
+	const Eigen::eigen2_Quaternion<Scalar> quat(Matrix3(globalTransform.block(0,0,3,3)));
 	
 	tf::Quaternion tfQuat;
-	tf::RotationEigenToTF(Eigen::Quaterniond(quat), tfQuat);
+	tf::RotationEigenToTF(quat.cast<double>(), tfQuat);
 	tf::Vector3 tfVect;
 	tf::VectorEigenToTF(globalTransform.block(0,3,3,1).cast<double>(), tfVect);
 	tf::Transform transform;
