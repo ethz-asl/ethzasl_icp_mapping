99121aead780639a9a1cedb1bf3a5be4d48cf2ca
Stéphane Magnenat <stephane@magnenat.net>
2011-06-09 11:29:42 +0200

    Cleaned-up bag-based tracker, now use libargtable2 for command line parsing.

--- modular_cloud_matcher/src/modular_cloud_matcher_bag.cpp
+++ modular_cloud_matcher/src/modular_cloud_matcher_bag.cpp
@@ -22,6 +22,9 @@
 #include "pcl/point_types.h"
 #include "pcl/point_cloud.h" 
 #include "pcl/ros/conversions.h"
+#include "sensor_msgs/CameraInfo.h"
+#include <argtable2.h>
+#include <regex.h>      /* REG_ICASE */
 
 using namespace std;
 using namespace Eigen;
@@ -49,6 +52,44 @@ T getParam(const std::string& name, const T& defaultValue)
 	}
 }
 
+void reloadParamsFromLine(ifstream& ifs)
+{
+	char line[65536];
+	ifs.getline(line, sizeof(line));
+	params.clear();
+	istringstream iss(line);
+	while (iss.good())
+	{
+		string keyVal;
+		iss >> keyVal;
+		if (!keyVal.empty())
+		{
+			const size_t delPos = keyVal.find_first_of('=');
+			if (delPos != string::npos)
+			{
+				const string key = keyVal.substr(0, delPos);
+				const string val = keyVal.substr(delPos+1);
+				params[key] = val;
+			}
+		}
+	}
+}
+
+void openWriteFile(ofstream& stream, arg_file * file, const int errorVal)
+{
+	if (file->count > 0)
+	{
+		stream.open(file->filename[0]);
+		if (!stream.good())
+		{
+			cerr << "Error, invalid " << file->hdr.glossary << " " << file->filename[0] << endl;
+			exit (errorVal);
+		}
+		else
+			cout << "Writing to " << file->hdr.glossary << " " << file->filename[0] << endl;
+	}
+}
+
 /** Inherits from message_filters::SimpleFilter<M>
     to use protected signalMessage function */
 template <class M>
@@ -67,6 +108,7 @@ struct Datum
 {
 	TP transform;
 	DP cloud;
+	ros::Time stamp;
 };
 
 typedef vector<Datum> Data;
@@ -87,43 +129,94 @@ TP msgTransformToTP(const geometry_msgs::Transform& transform)
 
 int main(int argc, char **argv)
 {
-	if (argc < 11)
+	arg_file * bagFile = arg_file1(NULL, NULL, "<bag>", "bag file");
+	arg_str  * cloudTopic = arg_str1(NULL, NULL, "<cloud_topic>", "cloud topic");
+	arg_file * paramsFile = arg_file1(NULL, NULL, "<params>", "params file");
+	arg_file * statFile = arg_file0(NULL, "stat", NULL, "statistic output file");
+	
+	arg_rex  * cmdGt = arg_rex1(NULL, NULL, "gt", NULL, REG_ICASE, "use ground truth");
+
+	arg_str  * gtTargetFrame = arg_str1(NULL, "gt_target_frame", NULL, "tf target frame of ground truth");
+	arg_str  * gtSourceFrame = arg_str1(NULL, "gt_source_frame", NULL, "tf source frame of ground truth");
+	
+	arg_dbl  * t_x = arg_dbl0(NULL, "t_x", NULL, "correction from gt to icp, translation on x");
+	arg_dbl  * t_y = arg_dbl0(NULL, "t_y", NULL, "correction from gt to icp, translation on y");
+	arg_dbl  * t_z = arg_dbl0(NULL, "t_z", NULL, "correction from gt to icp, translation on z");
+	arg_dbl  * q_x = arg_dbl0(NULL, "q_x", NULL, "correction from gt to icp, quaternion component x");
+	arg_dbl  * q_y = arg_dbl0(NULL, "q_y", NULL, "correction from gt to icp, quaternion component y");
+	arg_dbl  * q_z = arg_dbl0(NULL, "q_z", NULL, "correction from gt to icp, quaternion component z");
+	arg_dbl  * q_w = arg_dbl0(NULL, "q_w", NULL, "correction from gt to icp, quaternion component w");
+	
+	arg_file * tfFile = arg_file0(NULL, "tf", NULL, "tf output file");
+	arg_file * dtfFile = arg_file0(NULL, "dtf", NULL, "delta tf output file");
+	arg_int  * dtfSteps = arg_int0(NULL, "dtf_steps", NULL, "steps for computing delta tf (default: 30)");
+	struct arg_end  * end = arg_end(20);
+	
+	void *argTableCloud[] = {bagFile, cloudTopic, paramsFile, statFile, tfFile, end};
+	void* argTableCloudGt[] = {bagFile, cloudTopic, paramsFile, cmdGt, gtTargetFrame, gtSourceFrame, t_x, t_y, t_z, q_x, q_y, q_z, q_w, statFile, tfFile, dtfFile, dtfSteps, end};
+		
+	if (arg_nullcheck(argTableCloud) != 0 || 
+		arg_nullcheck(argTableCloudGt) != 0)
+		abort();
+	
+	const int nerrorsCloudGt = arg_parse(argc,argv,argTableCloudGt);
+	const int nerrorsCloud = arg_parse(argc,argv,argTableCloud);
+	if (nerrorsCloud > 0 && nerrorsCloudGt > 0)
 	{
-		cerr << "Usage: " << argv[0] << " bag params output t_x t_y t_z q_x q_y q_z q_w (transform from gt to icp, for correction) [tf_output] [delta_tf_output] [delta_tf_steps]" << endl;
+		cout << "Error(s) in command line:\n";
+		arg_print_errors(stdout,end,argv[0]);
+		cout << "\n";
+		cout << "Usage, for cloud without ground-truth:\n";
+		cout << argv[0];
+		arg_print_syntax(stdout,argTableCloud,"\n");
+		arg_print_glossary(stdout,argTableCloud,"  %-28s %s\n");
+		cout << "\n";
+		cout << "Usage, for cloud with ground-truth:\n";
+		cout << argv[0];
+		arg_print_syntax(stdout,argTableCloudGt,"\n");
+		arg_print_glossary(stdout,argTableCloudGt,"  %-28s %s\n");
 		return 1;
 	}
+	
+	const bool useGt = (nerrorsCloudGt == 0);
 	
 	srand(time(0));
 	
 	initParameters();
-	
-	// read correction
+
+	// setup correction
+	if (useGt)
 	{
 		Eigen::Vector3f tr;
-		tr(0) = atof(argv[4]);
-		tr(1) = atof(argv[5]);
-		tr(2) = atof(argv[6]);
+		tr(0) = t_x->count > 0 ? t_x->dval[0] : 0;
+		tr(1) = t_y->count > 0 ? t_y->dval[0] : 0;
+		tr(2) = t_z->count > 0 ? t_z->dval[0] : 0;
 		Eigen::eigen2_Quaternionf rot;
-		rot.x() = atof(argv[7]);
-		rot.y() = atof(argv[8]);
-		rot.z() = atof(argv[9]);
-		rot.w() = atof(argv[10]);
+		rot.x() = q_x->count > 0 ? q_x->dval[0] : 0;
+		rot.y() = q_y->count > 0 ? q_y->dval[0] : 0;
+		rot.z() = q_z->count > 0 ? q_z->dval[0] : 0;
+		rot.w() = q_w->count > 0 ? q_w->dval[0] : 1;
 		T_k_to_v = (Eigen::eigen2_Translation3f(tr) * rot).matrix();
 		T_v_to_k = T_k_to_v.inverse();
-		cout << "Using correction:\n";
+		cout << "Using ground-truth with correction:\n";
 		cout << T_k_to_v << "\n";
 	}
+	else
+		cout << "Not using ground-truth" << endl;
 	
 	// load data
 	try
 	{
+		sensor_msgs::CameraInfo camInfo;
+		
 		// 1 year of buffer
 		tf2::BufferCore tfBuffer(ros::Duration(31536000,0));
 		//TP firstTP;
 		//bool gotFirstTP(false);
+		if (useGt)
 		{
 			// first pass, read tf
-			rosbag::Bag bag(argv[1]);
+			rosbag::Bag bag(bagFile->filename[0]);
 			
 			rosbag::View view(bag, rosbag::TopicQuery("/tf"));
 			BOOST_FOREACH(rosbag::MessageInstance const m, view)
@@ -147,33 +240,37 @@ int main(int argc, char **argv)
 		labels.push_back(DP::Label("y", 1));
 		labels.push_back(DP::Label("z", 1));
 		labels.push_back(DP::Label("pad", 1));
-		
+		// TODO: add if use point cloud
 		{
 			// second pass, read points
 			rosbag::Bag bag(argv[1]);
 			
-			rosbag::View view(bag, rosbag::TopicQuery("/camera/rgb/points"));
+			rosbag::View view(bag, rosbag::TopicQuery(cloudTopic->sval[0]));
 			BOOST_FOREACH(rosbag::MessageInstance const m, view)
 			{
 				sensor_msgs::PointCloud2::ConstPtr cloudMsg = m.instantiate<sensor_msgs::PointCloud2>();
 				
 				string err;
-				TP tp;
-				cout << "adding cloud with transform from /vicon_object to /world at time: " <<  cloudMsg->header.stamp <<  "..." << endl;
-				if (tfBuffer.canTransform("world", "vicon_object", cloudMsg->header.stamp, &err))
+				TP tp = TP::Identity(4,4);
+				if (useGt)
 				{
-					const geometry_msgs::TransformStamped ts = tfBuffer.lookupTransform("world", "vicon_object", cloudMsg->header.stamp);
-					tp = msgTransformToTP(ts.transform);
-					cout << "success" << endl;
-				}
-				else
-				{
-					cout << "error" << endl;
-					continue;
+					cout << "adding cloud with transform from " << gtSourceFrame->sval[0] << " to " << gtTargetFrame->sval[0] << " at time: " <<  cloudMsg->header.stamp <<  "..." << endl;
+					if (tfBuffer.canTransform(gtTargetFrame->sval[0], gtSourceFrame->sval[0], cloudMsg->header.stamp, &err))
+					{
+						const geometry_msgs::TransformStamped ts = tfBuffer.lookupTransform(gtTargetFrame->sval[0], gtSourceFrame->sval[0], cloudMsg->header.stamp);
+						tp = msgTransformToTP(ts.transform);
+						cout << "success" << endl;
+					}
+					else
+					{
+						cout << "error" << endl;
+						continue;
+					}
 				}
 				
 				Datum datum;
 				datum.transform = tp;
+				datum.stamp = cloudMsg->header.stamp;
 				
 				// create data points
 				pcl::PointCloud<pcl::PointXYZ> cloud;
@@ -185,7 +282,10 @@ int main(int argc, char **argv)
 				int dIndex(0);
 				for (size_t i = 0; i < pointCount; ++i)
 				{
-					if (!isnan(cloud.points[i].x))
+					if (!isnan(cloud.points[i].x) && 
+						!isnan(cloud.points[i].y) &&
+						!isnan(cloud.points[i].z)
+					)
 					{
 						tempCloud(0, dIndex) = cloud.points[i].x;
 						tempCloud(1, dIndex) = cloud.points[i].y;
@@ -195,6 +295,7 @@ int main(int argc, char **argv)
 					}
 					else
 					{
+						//cout << i << " : " << cloud.points[i].x <<", " << cloud.points[i].y << ", " << cloud.points[i].z << endl;
 						//cout << "nan at pos " << j << "\n";
 					}
 				}
@@ -212,73 +313,34 @@ int main(int argc, char **argv)
 	}
 	
 	// load param list
-	
-	// open file
-	ifstream ifs(argv[2]);
+	ifstream ifs(paramsFile->filename[0]);
 	if (!ifs.good())
 	{
-		cerr << "Error, invalid params file " << argv[2] << endl;
+		cerr << "Error, invalid params file " << paramsFile->filename[0] << endl;
 		return 3;
 	}
-	ofstream ofs(argv[3]);
-	if (!ofs.good())
-	{
-		cerr << "Error, invalid output file " << argv[3] << endl;
-		return 4;
-	}
 	
-	// tf output file
-	ofstream tfofs;
-	if (argc >= 12)
-	{
-		tfofs.open(argv[11]);
-		if (!tfofs.good())
-		{
-			cerr << "Error, invalid tf output file " << argv[4] << endl;
-			return 5;
-		}
-	}
-	ofstream dtfofs;
-	if (argc >= 13)
-	{
-		dtfofs.open(argv[12]);
-		if (!tfofs.good())
-		{
-			cerr << "Error, invalid delta tf output file " << argv[5] << endl;
-			return 6;
-		}
-	}
+	// open stat and tf output files, if requested
+	ofstream statofs, tfofs, dtfofs;
+	openWriteFile(statofs, statFile, 4);
+	openWriteFile(tfofs, tfFile, 5);
+	openWriteFile(dtfofs, dtfFile, 6);
+	
 	int deltaTfSteps(30);
-	if (argc >= 14)
+	if (useGt && dtfSteps->count > 0)
 	{
-		deltaTfSteps = atoi(argv[13]);
+		deltaTfSteps = dtfSteps->ival[0];
 		cout << "Using tf steps of " << deltaTfSteps << endl;
 	}
+	
+	cout << endl;
 	
 	// for each line in the experiment file
 	unsigned expCount(0);
 	while (ifs.good())
 	{
 		// read line and load params
-		char line[65536];
-		ifs.getline(line, sizeof(line));
-		params.clear();
-		istringstream iss(line);
-		while (iss.good())
-		{
-			string keyVal;
-			iss >> keyVal;
-			if (!keyVal.empty())
-			{
-				const size_t delPos = keyVal.find_first_of('=');
-				if (delPos != string::npos)
-				{
-					const string key = keyVal.substr(0, delPos);
-					const string val = keyVal.substr(delPos+1);
-					params[key] = val;
-				}
-			}
-		}
+		reloadParamsFromLine(ifs);
 		if (params.empty())
 			break;
 		cout << "Exp " << expCount << ", loaded " << params.size() << " parameters\n\n";
@@ -298,16 +360,16 @@ int main(int argc, char **argv)
 		d = data[0].cloud;
 		unsigned failCount(0);
 		try
-    {
-      icp(d);
-    }
-    catch (MSA::ConvergenceError error)
-    {
-      ++failCount;
-      cerr << "ICP failed to converge at cloud 0 : " << error.what() << endl;
-    }
+		{
+			icp(d);
+		}
+		catch (MSA::ConvergenceError error)
+		{
+			++failCount;
+			cerr << "ICP failed to converge at cloud 0 : " << error.what() << endl;
+		}
 		
-    TP T_icp_old(T_gt_init * T_k_to_v * icp.getTransform() * T_v_to_k);
+		TP T_icp_old(T_gt_init * T_k_to_v * icp.getTransform() * T_v_to_k);
 		
 		TP T_d_gt_acc(TP::Identity(4,4));
 		TP T_d_icp_acc(TP::Identity(4,4));
@@ -339,16 +401,21 @@ int main(int argc, char **argv)
 			/*cerr << "T_gt_init:\n" << T_gt_init << "\nT_gt:\n" << T_gt << "\nT_k_to_v:\n" << T_k_to_v << "\nicp::getTransform:\n" << icp.getTransform() << "\nT_v_to_k:\n" << T_v_to_k << "\n\nT_icp:\n" << T_icp << endl;
 			cerr << "det icp::getTransform: " << icp.getTransform().determinant() << ", det T_icp: " << T_icp .determinant() << endl;*/
 			
-			// write output
-			if (argc >= 12)
+			// write tf output
+			if (tfofs.good())
 			{
-				// tf
-				const Vector3 t_gt(T_gt.topRightCorner(3,1));
-				const Quaternion<Scalar> q_gt(Matrix3(T_gt.topLeftCorner(3,3)));
-				tfofs << t_gt(0) << " " << t_gt(1) << " " << t_gt(2) << " " << q_gt.x() << " " << q_gt.y() << " " << q_gt.z() << " " << q_gt.w() << " ";
+				tfofs << data[i].stamp << " ";
+				
 				const Vector3 t_icp(T_icp.topRightCorner(3,1));
 				const Quaternion<Scalar> q_icp(Matrix3(T_icp.topLeftCorner(3,3)));
 				tfofs << t_icp(0) << " " << t_icp(1) << " " << t_icp(2) << " " << q_icp.x() << " " << q_icp.y() << " " << q_icp.z() << " " << q_icp.w() << "\n";
+				
+				if (useGt)
+				{
+					const Vector3 t_gt(T_gt.topRightCorner(3,1));
+					const Quaternion<Scalar> q_gt(Matrix3(T_gt.topLeftCorner(3,3)));
+					tfofs << t_gt(0) << " " << t_gt(1) << " " << t_gt(2) << " " << q_gt.x() << " " << q_gt.y() << " " << q_gt.z() << " " << q_gt.w() << " ";
+				}
 			}
 			
 			// dump deltas
@@ -388,7 +455,7 @@ int main(int argc, char **argv)
 				e_acc_a.push_back(2 * acos(quat.normalized().w()));
 				
 				// dump deltas
-				if (argc >= 13)
+				if (dtfofs.good())
 				{
 					// delta tf
 					const Vector3 t_gt(T_d_gt_acc.topRightCorner(3,1));
@@ -417,33 +484,39 @@ int main(int argc, char **argv)
 		
 		// write back results
 		// general stats
-		ofs << data.size() - 1 << " " << failCount << " * ";
-		// error on each dt
-		e_x.dumpStats(ofs); ofs << " * ";
-		e_y.dumpStats(ofs); ofs << " * ";
-		e_z.dumpStats(ofs); ofs << " * ";
-		e_a.dumpStats(ofs); ofs << " * ";
-		// error every sect
-		e_acc_x.dumpStats(ofs); ofs << " * ";
-		e_acc_y.dumpStats(ofs); ofs << " * ";
-		e_acc_z.dumpStats(ofs); ofs << " * ";
-		e_acc_a.dumpStats(ofs); ofs << " * ";
-		// timing
-		ofs << icpTotalDuration << " * ";
-		icp.keyFrameDuration.dumpStats(ofs); ofs << " * ";
-		icp.convergenceDuration.dumpStats(ofs); ofs << " * ";
-		// algo stats
-		icp.iterationsCount.dumpStats(ofs); ofs << " * ";
-		icp.pointCountIn.dumpStats(ofs); ofs << " * ";
-		icp.pointCountReading.dumpStats(ofs); ofs << " * ";
-		icp.pointCountKeyFrame.dumpStats(ofs); ofs << " * ";
-		icp.pointCountTouched.dumpStats(ofs); ofs << " * ";
-		icp.overlapRatio.dumpStats(ofs); ofs << " # ";
-		// params for info
-		for (Params::const_iterator it(params.begin()); it != params.end(); ++it)
-			ofs << it->first << "=" << it->second << " ";
-		// finish results
-		ofs << "\n";
+		if (statofs.good())
+		{
+			statofs << data.size() - 1 << " " << failCount << " * ";
+			if (useGt)
+			{
+				// error on each dt
+				e_x.dumpStats(statofs); statofs << " * ";
+				e_y.dumpStats(statofs); statofs << " * ";
+				e_z.dumpStats(statofs); statofs << " * ";
+				e_a.dumpStats(statofs); statofs << " * ";
+				// error every sect
+				e_acc_x.dumpStats(statofs); statofs << " * ";
+				e_acc_y.dumpStats(statofs); statofs << " * ";
+				e_acc_z.dumpStats(statofs); statofs << " * ";
+				e_acc_a.dumpStats(statofs); statofs << " * ";
+			}
+			// timing
+			statofs << icpTotalDuration << " * ";
+			icp.keyFrameDuration.dumpStats(statofs); statofs << " * ";
+			icp.convergenceDuration.dumpStats(statofs); statofs << " * ";
+			// algo stats
+			icp.iterationsCount.dumpStats(statofs); statofs << " * ";
+			icp.pointCountIn.dumpStats(statofs); statofs << " * ";
+			icp.pointCountReading.dumpStats(statofs); statofs << " * ";
+			icp.pointCountKeyFrame.dumpStats(statofs); statofs << " * ";
+			icp.pointCountTouched.dumpStats(statofs); statofs << " * ";
+			icp.overlapRatio.dumpStats(statofs); statofs << " # ";
+			// params for info
+			for (Params::const_iterator it(params.begin()); it != params.end(); ++it)
+				statofs << it->first << "=" << it->second << " ";
+			// finish results
+			statofs << "\n";
+		}
 	}
 
 	return 0;
