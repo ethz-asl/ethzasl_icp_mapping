08b2454f493fe9c3870684a7d7a905668fa5a67a
Stéphane Magnenat <stephane@magnenat.net>
2011-10-06 17:20:53 +0200

    Using improved logger structure.

--- modular_cloud_matcher/src/modular_cloud_matcher.cpp
+++ modular_cloud_matcher/src/modular_cloud_matcher.cpp
@@ -18,9 +18,11 @@ namespace PointMatcherSupport
 	struct ROSLogger: public Logger
 	{
 		virtual bool hasInfoChannel() const{ return true; };
+		virtual void beginInfoEntry(const char *file, unsigned line, const char *func);
 		virtual std::ostream* infoStream() { return &_infoStream; }
 		virtual void finishInfoEntry(const char *file, unsigned line, const char *func);
 		virtual bool hasWarningChannel() const { return true; }
+		virtual void beginWarningEntry(const char *file, unsigned line, const char *func);
 		virtual std::ostream* warningStream() { return &_warningStream; }
 		virtual void finishWarningEntry(const char *file, unsigned line, const char *func);
 		
@@ -31,16 +33,24 @@ namespace PointMatcherSupport
 		std::ostringstream _warningStream;
 	};
 	
+	void ROSLogger::beginInfoEntry(const char *file, unsigned line, const char *func)
+	{
+		_infoStream.str("");
+	}
+	
 	void ROSLogger::finishInfoEntry(const char *file, unsigned line, const char *func)
 	{
 		writeRosLog(ros::console::levels::Info, file, line, func, _infoStream.str());
-		_infoStream.str("");
+	}
+	
+	void ROSLogger::beginWarningEntry(const char *file, unsigned line, const char *func)
+	{
+		_warningStream.str("");
 	}
 	
 	void ROSLogger::finishWarningEntry(const char *file, unsigned line, const char *func)
 	{
 		writeRosLog(ros::console::levels::Warn, file, line, func, _warningStream.str());
-		_warningStream.str("");
 	}
 	
 	void ROSLogger::writeRosLog(ros::console::Level level, const char* file, int line, const char *func, const string& text)
