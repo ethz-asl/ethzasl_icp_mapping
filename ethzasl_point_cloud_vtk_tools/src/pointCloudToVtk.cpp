d2f9327eb1079857d3bb2b30016d5d917c321748
smichaud <smichaud@smichaud-Z68A-D3H-B3.(none)>
2014-08-26 13:31:57 -0400

    Added a parameter to select  where the point clouds are saved (for the pointCloudToVtk node)

--- ethzasl_point_cloud_vtk_tools/src/pointCloudToVtk.cpp
+++ ethzasl_point_cloud_vtk_tools/src/pointCloudToVtk.cpp
@@ -22,82 +22,93 @@ using namespace PointMatcherSupport;
 
 class ExportVtk
 {
-	typedef PointMatcher<float> PM;
-	typedef PM::DataPoints DP;
-	
-	ros::NodeHandle& n;
-	
-	ros::Subscriber cloudSub;
-	const string cloudTopic;
-	const string mapFrame;
-  const bool recordOnce;
+    typedef PointMatcher<float> PM;
+    typedef PM::DataPoints DP;
 
-	tf::TransformListener tf_listener;
-	std::shared_ptr<PM::Transformation> transformation;
+    ros::NodeHandle& n;
+
+    ros::Subscriber cloudSub;
+    const string cloudTopic;
+    const string mapFrame;
+    const bool recordOnce;
+    const string outputPath;
+
+    tf::TransformListener tf_listener;
+    std::shared_ptr<PM::Transformation> transformation;
 
 public:
-	ExportVtk(ros::NodeHandle& n);
-	void gotCloud(const sensor_msgs::PointCloud2& cloudMsg);
+    ExportVtk(ros::NodeHandle& n);
+    void gotCloud(const sensor_msgs::PointCloud2& cloudMsg);
 };
 
 ExportVtk::ExportVtk(ros::NodeHandle& n):
-	n(n),
-	cloudTopic(getParam<string>("cloudTopic", "/static_point_cloud")),
-	mapFrame(getParam<string>("mapFrameId", "/map")),
-	recordOnce(getParam<bool>("recordOnce", "false")),
-	transformation(PM::get().TransformationRegistrar.create("RigidTransformation"))
+    n(n),
+    cloudTopic(getParam<string>("cloudTopic", "/static_point_cloud")),
+    mapFrame(getParam<string>("mapFrameId", "/map")),
+    recordOnce(getParam<bool>("recordOnce", "false")),
+    outputPath(getParam<string>("outputPath", "")),
+    transformation(PM::get().TransformationRegistrar.create("RigidTransformation"))
 {
-	// ROS initialization
-	cloudSub = n.subscribe(cloudTopic, 100, &ExportVtk::gotCloud, this);
-	
-	// Parameters for 3D map
+    // ROS initialization
+    cloudSub = n.subscribe(cloudTopic, 100, &ExportVtk::gotCloud, this);
+
+    // Parameters for 3D map
 }
 
 
 void ExportVtk::gotCloud(const sensor_msgs::PointCloud2& cloudMsgIn)
 {
-	const DP inCloud(PointMatcher_ros::rosMsgToPointMatcherCloud<float>(cloudMsgIn));
-	
-  try
-  {
-    const PM::TransformationParameters tr(PointMatcher_ros::transformListenerToEigenMatrix<float>(tf_listener,  mapFrame, cloudMsgIn.header.frame_id, cloudMsgIn.header.stamp));
-    
-    const DP outCloud(transformation->compute(inCloud, tr));
-    
-    if (outCloud.features.cols() == 0)
+    const DP inCloud(PointMatcher_ros::rosMsgToPointMatcherCloud<float>(cloudMsgIn));
+
+    try
     {
-      ROS_ERROR("I found no good points in the cloud");
-      return;
+        const PM::TransformationParameters tr(PointMatcher_ros::transformListenerToEigenMatrix<float>(tf_listener,  mapFrame, cloudMsgIn.header.frame_id, cloudMsgIn.header.stamp));
+
+        const DP outCloud(transformation->compute(inCloud, tr));
+
+        if (outCloud.features.cols() == 0)
+        {
+            ROS_ERROR("I found no good points in the cloud");
+            return;
+        }
+        else
+        {
+            ROS_INFO("Saving cloud");
+        }
+
+        stringstream nameStream;
+        if(outputPath.empty())
+        {
+            nameStream << ".";
+        }
+        else
+        {
+            nameStream << outputPath;
+        }
+        nameStream << cloudTopic << "_" << cloudMsgIn.header.seq << ".vtk";
+
+        outCloud.save(nameStream.str());
+        if(recordOnce)
+        {
+            ros::shutdown();
+        }
     }
-    else
+    catch (tf::ExtrapolationException e)
     {
-      ROS_INFO("Saving cloud");
+        ROS_WARN("Still build tf tree, skipping that scan");
     }
-    
-    stringstream nameStream;
-    nameStream << "." << cloudTopic << "_" << cloudMsgIn.header.seq << ".vtk";
-    outCloud.save(nameStream.str());
-    if(recordOnce)
-    {
-      ros::shutdown();  
-    }
-  }
-  catch (tf::ExtrapolationException e)
-  {
-    ROS_WARN("Still build tf tree, skipping that scan");  
-  }
 }
 
 // Main function supporting the ExportVtk class
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "pointCloudToVtk_node");
-	ros::NodeHandle n;
-	ExportVtk exporter(n);
-	ros::spin();
+    ros::init(argc, argv, "pointCloudToVtk_node");
+    ros::NodeHandle n;
+    ExportVtk exporter(n);
+    ros::spin();
 
-  // Wait for the shutdown to finish
-  while(ros::ok()){}
-	
-	return 0;
+    // Wait for the shutdown to finish
+    while(ros::ok()){}
+
+    return 0;
 }
